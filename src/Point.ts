b1b545152ccb8d9f78dbd7ee4332f8c9b239355f
William Mbotta <sergembotta@mailoo.org>
2022-04-12 21:42:27 +0200

    add add method to Point

--- src/Point.ts
+++ src/Point.ts
@@ -8,8 +8,48 @@ export default class Point {
     this.b = b
     this.x = x
     this.y = y
-    if (Math.pow(this.y, 2) !== Math.pow(this.x, 3) + a * x + b)
+
+    if (this.x === Infinity && this.y === Infinity) {
+      return
+    }
+
+    if (this.y ** 2 !== this.x ** 3 + a * x + b)
       throw Error(`(${x}, ${y}) is not on the curve`)
+  }
+
+  add(other: Point): Point {
+    if (this.a !== other.a || this.b !== other.b) {
+      throw Error(`Points ${this}, ${other} are not on the same curve`)
+    }
+
+    if (this.x === Infinity) {
+      return other
+    }
+    if (other.x === Infinity) {
+      return this
+    }
+
+    if (this.x == other.x && this.y != other.y) {
+      return new Point(Infinity, Infinity, this.a, this.b)
+    }
+
+    if (this.x != other.x) {
+      const s = (other.y - this.y) / (other.x - this.x)
+      const x = s ** 2 - this.x - other.x
+      const y = s * (this.x - x) - this.y
+      return new Point(x, y, this.a, this.b)
+    }
+
+    if (this == other && this.y == 0 * this.x) {
+      return new Point(Infinity, Infinity, this.a, this.b)
+    }
+
+    if (this === other) {
+      let s = (3 * this.x ** 2 + this.a) / (2 * this.y)
+      let x = s ** 2 - 2 * this.x
+      let y = s * (this.x - x) - this.y
+      return new Point(x, y, this.a, this.b)
+    }
   }
 
   eq(other: Point): boolean {
--- test/Point.test.ts
+++ test/Point.test.ts
@@ -7,4 +7,12 @@ describe('Point', () => {
     const q = () => new Point(-1, -1, 5, 7)
     expect(q).toBeDefined()
   })
+  test('add Point', () => {
+    let a = new Point(3, 7, 5, 7)
+    let b = new Point(-1, -1, 5, 7)
+    expect(a.add(b)).toEqual(new Point(2, -5, 5, 7))
+
+    const c = new Point(-1, 1, 5, 7)
+    expect(c.add(c)).toEqual(new Point(18, -77, 5, 7))
+  })
 })
